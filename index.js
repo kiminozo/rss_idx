6cdadc6a31e5e35bcddafdff4ae6580f83dbcff8
kiminozo <kiminozoeien@gmail.com>
2019-10-23 16:46:56 +0800

    pipe

--- index.js
+++ index.js
@@ -1,64 +1,51 @@
 const https = require("https")
+const request = require("request")
 const Koa = require('koa')
 const route = require('koa-route');
+const MemoryStream = require('memorystream');
+
 
 const app = new Koa()
 
 const acg_rss_url = 'https://acg.rip/.xml?term='
 const dmhy_rss_url = 'https://share.dmhy.org/topics/rss/rss.xml?keyword='
-// https.get(test_rss, res => {
-//     console.log(`Got response: ${res.statusCode}`);
-//     let html = ""
 
-//     res.on("data", (data) => {
-//         html += data
-//     })
-
-//     res.on("end", () => {
-//         console.log(html)
-//     })
-// })
-
-function getRss(url) {
+function getBody(url) {
     return new Promise((resolve, reject) => {
-        https.get(url, res => {
-            console.log(`Got response: ${res.statusCode}`);
-            let html = ""
+        let stream = new MemoryStream();
 
-            res.on("data", (data) => {
-                html += data
+        request.get(url)
+            .on('close', () => {
+                console.log("close");
+                resolve(stream);
             })
-
-            res.on("end", () => {
-                resolve(html);
-                //console.log(html)
+            .on("error", e => {
+                console.error(e);
+                reject(e);
             })
-        }).on("error", e => {
-            console.error(e);
-            reject(e);
-        })
+            .pipe(stream)
     })
-
 }
 
 
 const main = ctx => {
     ctx.response.body = 'Hello World '
 };
-const test = ctx => {
-    let key = ctx.request.query.key;
-    ctx.response.body = 'key= ' + key;
-};
+const test = async ctx => {
+    ctx.response.type = 'text/html';
+    ctx.response.body = await getBody('https://www.google.com');
+}
 
 async function proxy(ctx, base_url) {
     let key = ctx.request.query.key;
     console.log('key= ' + key)
     try {
-        ctx.response.body = await getRss(dmhy_rss_url + key);
         ctx.response.type = 'application/xml; charset=utf-8';
+        ctx.response.body = await getBody(base_url + encodeURI(key));
     } catch (error) {
-        ctx.response.code = 504;
-        ctx.response.body = error.stack;
+        ctx.response.code = 503;
+        ctx.response.type = 'text/html';
+        ctx.response.body = error.message;
     }
 
 }
@@ -71,3 +58,4 @@ app.use(route.get('/dmhy', async ctx => proxy(ctx, dmhy_rss_url)));
 
 app.listen(3000);
 console.log("service start http://localhost:3000/")
+//test()
